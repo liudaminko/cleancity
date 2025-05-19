5a3b3f391c683e7a284a392055e1205eebb993df
liudmyla minkovets <luda.min2003@gmail.com>
2025-05-19 11:40:35 +0300

    Initial Commit

---
+++ Clean.City/Clean_CityApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clean_CityApp.swift
+//  Clean.City
+//
+//  Created by Liudmyla Minkovets on 19.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct Clean_CityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clean.CityTests/Clean_CityTests.swift
@@ -0,0 +1,17 @@
+//
+//  Clean_CityTests.swift
+//  Clean.CityTests
+//
+//  Created by Liudmyla Minkovets on 19.05.2025.
+//
+
+import Testing
+@testable import Clean_City
+
+struct Clean_CityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
